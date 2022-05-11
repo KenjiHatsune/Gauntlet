addfafe514140347e92f0bc9203863d9aeac7df2
pablo123w <95883542+pablo123w@users.noreply.github.com>
2022-05-11 15:36:38 -0700

    food and treasure script
    I made scripts for the food and treasure pickups and I was hoping you can check if they were done correctly.

---
+++ Gauntlet Clone/Assets/4) Scripts/Food_PickUp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food_PickUp : Adventurer
+{
+    public void FoodPick()
+    {
+        Health += 100;
+    }
+}
---
+++ Gauntlet Clone/Assets/4) Scripts/Treasure_PickUo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure_PickUo : Adventurer
+{
+    public void Treasure()
+    {
+        Score += 100;
+    }
+}
